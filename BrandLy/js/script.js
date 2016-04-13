0def0646943035934a39b0330f8c049b9a062470
nxaxg <nxayancanxg@gmail.com>
2016-04-12 21:19:11 -0300

    Some JS for the menu.

---
+++ BrandLy/js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    header();
+});
+
+
+
+//scrolling down
+function header() {
+    var tope = 500; /*from top*/
+
+    function posActual() {
+        return window.pageYOffset || document.documentElement.scrollTop;
+    }
+
+
+    $(window).scroll(function () {
+        var scroll = posActual(); /*posicion actual*/
+        if (scroll >= tope && scroll < 1400) {
+            smallHeader();
+            linkRemove();
+            $('nav ul li:nth-child(2)>a').addClass('nav-selected');
+        } else if (scroll >= 1400 && scroll < 2100) {
+            smallHeader();
+            linkRemove();
+            $('nav ul li:nth-child(3)>a').addClass('nav-selected');
+        } else if (scroll >= 2100 && scroll < 2300) {
+            smallHeader();
+            linkRemove();
+            $('nav ul li:nth-child(4)>a').addClass('nav-selected');
+        } else if (scroll >= 2300) {
+            smallHeader();
+            linkRemove();
+            $('nav ul li:nth-child(5)>a').addClass('nav-selected');
+        } else {
+            $('header').removeClass('small-header');
+            linkRemove();
+            $('header ul li:first-child>a').addClass('nav-selected');
+        }
+    });
+
+    function smallHeader() {
+        return $('header').addClass('small-header');
+    }
+
+    function linkRemove() {
+        return $('.menu-link').removeClass('nav-selected');
+    }
+};
